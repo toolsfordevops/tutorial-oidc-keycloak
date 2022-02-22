7cf0e98df525b919182a4d31a3c3108f3b8d80f9
Wassim Akachi <wassim@bytesandcodes.com>
2022-02-22 17:26:43 +0100

    add keycloak (16.1) based on wildfly

--- keycloak-wildfly/reset-infra.sh
+++ keycloak-wildfly/reset-infra.sh
@@ -4,5 +4,7 @@ HERE_FOLDER=$(dirname "$0")
 ROOT_FOLDER=$(cd "$HERE_FOLDER" && cd ../ && pwd)
 
 $ROOT_FOLDER/keycloak-wildfly/stop-infra.sh && \
-rm -rf "$ROOT_FOLDER/keycloak-wildfly/docker/oidc-sql-database-mounted" && \
-$ROOT_FOLDER/keycloak-wildfly/start-infra.sh
+rm -rf "$ROOT_FOLDER/keycloak-wildfly/docker/oidc-sql-database-mounted"
+
+
+# $ROOT_FOLDER/keycloak-wildfly/start-infra.sh
