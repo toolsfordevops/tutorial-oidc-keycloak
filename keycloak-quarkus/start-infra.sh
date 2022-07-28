25c721fc3486af534c58c2bc5c1f9a88c1c03f83
Wassim Akachi <wassim@bytesandcodes.com>
2022-07-28 20:20:08 +0200

    update scripts to be compatible with docker-compose

--- keycloak-quarkus/start-infra.sh
+++ keycloak-quarkus/start-infra.sh
@@ -4,4 +4,4 @@ HERE_FOLDER=$(dirname "$0")
 ROOT_FOLDER=$(cd "$HERE_FOLDER" && cd ../ && pwd)
 
 mkdir -p "$ROOT_FOLDER/keycloak-quarkus/docker/oidc-sql-database-mounted"
-docker compose -f "$ROOT_FOLDER/keycloak-quarkus/docker-compose.yaml" up -d sql-database oidc-keycloak
+docker-compose -f "$ROOT_FOLDER/keycloak-quarkus/docker-compose.yaml" up -d sql-database oidc-keycloak
