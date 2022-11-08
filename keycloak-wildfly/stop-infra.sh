212f0ca9ab91f8e5f21c7db3fed3710cae0724d0
Wassim Akachi <Wassim@BytesAndCodes.com>
2022-11-08 15:35:40 +0100

    fix shutdown command for wildfly-keycloak

--- keycloak-wildfly/stop-infra.sh
+++ keycloak-wildfly/stop-infra.sh
@@ -3,4 +3,4 @@
 HERE_FOLDER=$(dirname "$0")
 ROOT_FOLDER=$(cd "$HERE_FOLDER" && cd ../ && pwd)
 
-docker compose -f "$ROOT_FOLDER/keycloak-wildfly/docker-compose.yaml" down sql-database oidc-keycloak
+docker compose -f "$ROOT_FOLDER/keycloak-wildfly/docker-compose.yaml" down 
